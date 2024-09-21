0c43a22f49fdf2b1aededd7b947606e905d56bd8
Ali Sadeghi <alisadghi228@gmail.com>
2024-09-21 18:54:43 +0330

    feat : add the auth with cognito

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -5,6 +5,7 @@ import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import StoreProvider, { useAppSelector } from "./redux";
+import AuthProvider from "./authProvider";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   const isSidebarCollapsed = useAppSelector(
@@ -38,7 +39,9 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      <DashboardLayout>{children}</DashboardLayout>
+      <AuthProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </AuthProvider>
     </StoreProvider>
   );
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -34,8 +34,8 @@ app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
-const port = Number(process.env.PORT) || 3000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
